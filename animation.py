ecc2e5ca8e3d0cc2d2b6cec5154a9f4dac9162d0
adc2108 <alexandraciesielka@gmail.com>
2023-09-23 21:14:56 -0400

    mapping thigns

--- animation.py
+++ animation.py
@@ -1,6 +1,8 @@
 import pygame
 from gtts import gTTS
 import os
+import nltk
+from nltk.corpus import cmudict
 
 # Initialize Pygame
 pygame.init()
@@ -9,30 +11,59 @@ pygame.init()
 screen = pygame.display.set_mode((800, 800))
 
 # Load viseme images
-viseme_images = []
 viseme_dir = 'visemes'  # Directory containing viseme images
-for filename in sorted(os.listdir(viseme_dir)):
-    if filename.endswith(".png"):
-        viseme_image = pygame.image.load(os.path.join(viseme_dir, filename))
-        print(f"Loaded image: {filename}")
-        viseme_images.append(viseme_image)
-        print(viseme_images)
+viseme_images = [pygame.image.load(os.path.join(viseme_dir, filename)) for filename in sorted(os.listdir(viseme_dir)) if filename.endswith(".png")]
+
+# Load CMU Pronouncing Dictionary
+cmu_dict = cmudict.dict()
+
+# Define a function to map phonemes to viseme images
+def map_phoneme_to_viseme(phoneme):
+    # Define your mapping logic here
+    # Example: 'AA' maps to the first viseme image
+    viseme_mapping = {
+        'HH': viseme_images[0],
+        
+        'AH0': viseme_images[0],
+        'L': viseme_images[7],
+        'OW1': viseme_images[6],
+        'M': viseme_images[1],
+        'AY1': viseme_images[0],
+        'N': viseme_images[7],
+        'EY1': viseme_images[0],
+        'IH1' : viseme_images[0],
+        'Z' : viseme_images[2],
+        'AE': viseme_images[0],
+        'MBP': viseme_images[1],
+        'TS': viseme_images[2],
+        'WR': viseme_images[3],
+        'UQ': viseme_images[4],
+        'FV': viseme_images[5],
+        'O': viseme_images[6],
+        'LN': viseme_images[7],
+        # Add more mappings as needed
+    }
+    # Return the corresponding viseme image or a default image if not found
+    return viseme_mapping.get(phoneme, viseme_images[0])
 
 
 # Create a function to display a viseme image
 def display_viseme(viseme_image):
-    print("Displaying viseme image")
-    resized_image = pygame.transform.scale(viseme_image, (800, 600))  # Resize the image
+    print(viseme_image)
+    resized_image = pygame.transform.scale(viseme_image, (800, 800))  # Resize the image
     screen.fill((255, 255, 255))  # Clear the screen
     screen.blit(resized_image, (0, 0))  # Display the resized image
     pygame.display.flip()
-# Create a function to animate the face
-def animate_face(viseme_images):
-    for viseme_image in viseme_images:
-        display_viseme(viseme_image)
-        pygame.time.delay(500)  # Adjust the delay to control animation speed
 
-# Create a function to speak text and animate visemes
+# Create a function to animate the face based on phonemes
+def animate_face(phoneme_sequence):
+    for phoneme in phoneme_sequence:
+        # Map phoneme to viseme and display corresponding viseme image
+        print("phoneme" + phoneme)
+        viseme_image = map_phoneme_to_viseme(phoneme)
+        display_viseme(viseme_image)
+        pygame.time.delay(155)  # Adjust the delay to control animation speed
+# Create a function to speak text, convert to phonemes, and animate visemes
 def speak_and_animate(text):
     # Generate speech using gTTS
     tts = gTTS(text)
@@ -43,27 +74,30 @@ def speak_and_animate(text):
     pygame.mixer.music.load("output.mp3")
     pygame.mixer.music.play()
 
-    # Animate the face
-    animate_face(viseme_images)
+    # Convert text to phonemes
+    phoneme_sequence = text_to_phonemes(text)
+
+    # Animate the face based on phonemes
+    animate_face(phoneme_sequence)
 
     # Wait for the speech to finish
     pygame.mixer.music.queue("output.mp3")
 
     pygame.quit()
 
+# Define a function to convert text to phonemes
+def text_to_phonemes(text):
+    text = text.lower()  # Convert text to lowercase
+    words = nltk.word_tokenize(text)
+    phoneme_sequence = []
+
+    for word in words:
+        if word in cmu_dict:
+            phonemes = cmu_dict[word][0]
+            phoneme_sequence.extend(phonemes)
+    print("sequenc" + str(phoneme_sequence))
+    return phoneme_sequence
+
 # Example usage:
-text_to_speak = "Hello, my name is EcoBuddy!"
+text_to_speak = "Hello, my name is EcoBuddy! Nice to meet you"
 speak_and_animate(text_to_speak)
-
-# viseme_mapping = {
-#     'Open': pygame.image.load('images/AE.png'),
-#     'FFFF': pygame.image.load('images/FV.png'),
-#     'Em': pygame.image.load('images/MBP.png'),
-#     'Ow': pygame.image.load('images/O.png'),
-#     'ST': pygame.image.load('images/TS.png'),
-#     'Cute': pygame.image.load('images/UQ.png'),
-#     'Were': pygame.image.load('images/WR.png')
-#     # Add more mappings as needed
-# }
-
-
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import pygame
+import animation
+import io
 
 app = Flask(__name__)
 
+# Initialize Pygame
+pygame.init()
+
+# Create a Pygame window and surface
+screen = pygame.display.set_mode((800, 800))
+
+# Route to serve the web page
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,6 +27,12 @@ def tab2():
 @app.route('/tab3')
 def tab3():
     return render_template('tab3.html')
+
+# Route to display the Pygame component on a web page
+@app.route('/pygame.html')
+def pygame_component():
+    animation.run_pygame_logic()
+    return "Pygame logic executed successfully"  # Customize the response as needed
 
 if __name__ == '__main__':
     app.run(debug=True)
