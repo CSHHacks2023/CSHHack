ee512661548e114887e669124b69d6289c690f32
adc2108 <alexandraciesielka@gmail.com>
2023-09-24 07:24:32 -0400

    Fixed Phoneme to visemes

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ import io
 
 app = Flask(__name__)
 
-# Initialize Pygame
-pygame.init()
+# # Initialize Pygame
+# pygame.init()
 
-# Create a Pygame window and surface
-screen = pygame.display.set_mode((800, 800))
+# # Create a Pygame window and surface
+# screen = pygame.display.set_mode((800, 800))
 
 # Route to serve the web page
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/tab1')
-def tab1():
-    return render_template('tab1.html')
+# @app.route('/tab1')
+# def tab1():
+#     return render_template('tab1.html')
 
 @app.route('/tab2')
 def tab2():
@@ -28,11 +28,11 @@ def tab2():
 def tab3():
     return render_template('tab3.html')
 
-# Route to display the Pygame component on a web page
-@app.route('/pygame.html')
-def pygame_component():
-    animation.run_pygame_logic()
-    return "Pygame logic executed successfully"  # Customize the response as needed
+# # Route to display the Pygame component on a web page
+# @app.route('/pygame.html')
+# def pygame_component():
+#     animation.run_pygame_logic()
+#     return "Pygame logic executed successfully"  # Customize the response as needed
 
 if __name__ == '__main__':
     app.run(debug=True)
