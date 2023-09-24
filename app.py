3275ef38e496982b79d764366a8f0d068e021d2b
adc2108 <alexandraciesielka@gmail.com>
2023-09-24 11:35:18 -0400

    Carbon tracker does its things

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ from flask import Flask, render_template, Response, request, jsonify
 
 app = Flask(__name__)
 
-# Initialize Pygame
-# pygame.init()
 
-# Create a Pygame window and surface
-# screen = pygame.display.set_mode((800, 800))
-
-# Route to serve the web page
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -29,12 +23,6 @@ def news():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-# Route to display the Pygame component on a web page
-# @app.route('/pygame.html')
-# def pygame_component():
-#     animation.run_pygame_logic()
-#     return "Pygame logic executed successfully"  # Customize the response as needed
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,15 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const driveButton = document.getElementById("drive");
     const carbonSpan = document.getElementById("carbon");
+
+    const carbonCounter = document.querySelector(".carbon-counter");
+
+
+
 
     // Initialize the carbon footprint value
     let carbonFootprint = 0;
-
-    // Add event listeners for the game options
-    driveButton.addEventListener("click", function () {
-        // Update the carbon footprint
-        carbonFootprint += 1; // Increment by 1 for demonstration purposes
-        // Update the carbon footprint display
+    function updateCarbonDisplay() {
         carbonSpan.innerText = carbonFootprint;
+    }
+
+    const ecoFriendlyStoreButton = document.querySelector(".button5");
+    const driveButton = document.querySelector(".button6");
+
+    const ecoFriendlyButton2= document.querySelector(".button7");
+    const driveButton2 = document.querySelector(".button8");
+
+    const ecoFriendlyButton3 = document.querySelector(".button9");
+    const driveButton3 = document.querySelector(".button10");
+
+    const ecoFriendlyButton4 = document.querySelector(".button11");
+    const driveButton4 = document.querySelector(".button12");
+
+    ecoFriendlyStoreButton.addEventListener("click", function () {
+        carbonFootprint -= 1; // Decrement the carbon footprint
+        updateCarbonDisplay();
     });
+
+    driveButton.addEventListener("click", function () {
+        carbonFootprint += 1; // Increment the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    ecoFriendlyButton2.addEventListener("click", function () {
+        carbonFootprint -= 1; // Decrement the carbon footprint
+        updateCarbonDisplay();
+    });
+
+
+    driveButton2.addEventListener("click", function () {
+        carbonFootprint += 1; // Increment the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    ecoFriendlyButton3.addEventListener("click", function () {
+        carbonFootprint -= 1; // Decrement the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    driveButton3.addEventListener("click", function () {
+        carbonFootprint += 1; // Increment the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    ecoFriendlyButton4.addEventListener("click", function () {
+        carbonFootprint -= 1; // Decrement the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    driveButton4.addEventListener("click", function () {
+        carbonFootprint += 1; // Increment the carbon footprint
+        updateCarbonDisplay();
+    });
+
+    function updateCarbonDisplay() {
+        carbonSpan.innerText = carbonFootprint;
+
+        // Check if carbonFootprint is 10 or more, and change background color
+        if (carbonFootprint >= 10) {
+            carbonCounter.style.backgroundColor = "#FF3333"; // Red color
+        } else {
+            carbonCounter.style.backgroundColor = ""; // Reset to default color
+        }
+    }
 });
